26be1afb987941cfd779eb2d49f38e92182f473e
railsfactory-sriman <sriman@railsfactory.org>
2011-08-12 14:46:05 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MeetlinkshareApi::Application.config.secret_token = 'ae96ea04105af9ee2b06862127f4a8dc25d29de69671d996a050598a850a296d1f41fa860f12b96a78e8d9e251affcbde577757ea644482bfb8ef1f0ba6c8770'

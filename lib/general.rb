8a99ba9454cac62dfbd3eccbb218e6b800bc8f20
senthilkumar <senthilkumar@sedin.co.in>
2011-08-12 16:41:42 +0530

    general methods

---
+++ lib/general.rb
@@ -0,0 +1,12 @@
+class TransformCode
+  class<<self
+    def encode_credentials(email,password)
+      join_credentials= [email.strip,password.strip].join(' ') #joins the email and password as a string with a space
+      Base64.encode64(join_credentials)
+    end
+    
+    def decode_credentials(string)
+      Base64.decode64(string).split #decodes the credentials as [email,password]
+    end
+  end
+end

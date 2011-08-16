88c18899bd9bd843ccfcdee743b84bcf1477986a
railsfactory-soundar <soundar.rathinasamy@sedin.co.in>
2011-08-16 15:44:16 +0530

    added validation message

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User
   # Include default devise modules. Others available are:i
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :confirmable, :database_authenticatable, :registerable, :recoverable, :rememberable, :token_authenticatable, :trackable, :validatable
+  
+  validates_presence_of :first_name, :message=>"first_name - Blank Parameter", :code=>3010
+  validates_presence_of :last_name, :message=>"last_name - Blank Parameter", :code=>3011
+  
   field :first_name, :type=> String
   field :last_name, :type=>String
   field :job_title, :type=>String

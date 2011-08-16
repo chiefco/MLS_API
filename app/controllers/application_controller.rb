0f75998a8cc5ab758fae2ae51ec9611585aa85fd
senthilkumar <senthilkumar@sedin.co.in>
2011-08-16 19:24:55 +0530

    session removed for every request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
+  after_filter :clear_session
+
   USER_COLUMN=[:status,:remember_token,:remember_created_at,:created_at,:updated_at]
-  protect_from_forgery
+  #~ protect_from_forgery
+
+
+  def clear_session
+    session.clear
+  end
+
 end

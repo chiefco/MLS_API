f2ec08979fe4706858502d4401151f3fa8fe5a4e
railsfactory-soundar <soundar.rathinasamy@sedin.co.in>
2011-08-16 19:37:06 +0530

    changes in registration controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,6 @@ class RegistrationsController < Devise::RegistrationsController
   # POST /resource
   def create
     build_resource
-
     if resource.save
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_navigational_format?
@@ -11,7 +10,7 @@ class RegistrationsController < Devise::RegistrationsController
         respond_to do |format|
 					format.html
 					format.xml { render :xml=> resource }
-					format.json { render :json=>resource }
+					format.json { render :json=> resource }
 				end 
       else
         set_flash_message :notice, :inactive_signed_up, :reason => resource.inactive_message.to_s if is_navigational_format?
@@ -19,7 +18,7 @@ class RegistrationsController < Devise::RegistrationsController
         respond_to do |format|
 					format.html
 					format.xml { render :xml=> resource }
-					format.json { render :json=>resource }
+					format.json { render :json=> { "response" => "success", "status" => 200, "user" => { "email" => resource.email, "first_name" => resource.first_name, "last_name" => resource.last_name }} }
 				end
       end
     else

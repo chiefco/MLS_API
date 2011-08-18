cffdeeae209b26c3d5aeedab2c6200db07bcb9e7
senthilkumar <senthilkumar@sedin.co.in>
2011-08-18 19:15:04 +0530

    templates added

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,21 +9,24 @@ class Item
   field :status, :type => Boolean
   field :frequency_count, :type => Integer
   referenced_in :user
+  referenced_in :template
   validates_presence_of :name,:message=>'name - Required parameter missing',:code=>2009
-  
-  api_accessible :item_with_user do |template|
-    template.add :name
-    template.add :description
-    template.add :meet_date
-    template.add :_id
-    template.add :frequency_count
+  validates_presence_of :template_id,:message=>'template_id - Required parameter missing',:code=>2011
+  validates :template_fields
+
+  def template_fields
+    true
   end
   
-  api_accessible :item_detail do |template|
-    template.add :name
-    template.add :description
-    template.add :meet_date
-    template.add :_id
-    template.add :frequency_count
+  api_accessible :item_with_user do |t|
+    t.add :name
+    t.add :description
+    t.add :meet_date
+    t.add :_id
+    t.add :frequency_count
+  end
+  
+  api_accessible :item_detail,:extend=>:item_with_user do |t|
+    t.add 'user'
   end
 end

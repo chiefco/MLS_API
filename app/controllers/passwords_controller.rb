ba4b5f5e1ea8fb9008f3e9eacbad8e6ec61c065a
railsfactory-soundar <soundar.rathinasamy@sedin.co.in>
2011-08-19 11:06:29 +0530

    completed reset password API call

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -21,18 +21,17 @@ class PasswordsController < Devise::PasswordsController
   
   def update
     self.resource = resource_class.reset_password_by_token(params[resource_name])
-    if resource.errors.empty?
-      success_message=success.merge(RESET_TOKEN_SENT)
+    if self.resource.errors.empty?
       respond_to do |format|
-        format.xml{ render :xml=>success_message, :root => :result}
-        format.json{render :json=>success_message.to_json}
+        format.xml{ render :xml=>success, :root => :result}
+        format.json{render :json=>success.to_json}
       end
     else
-      failure_message=failure.merge(RESET_TOKEN_ERROR)
       respond_to do |format|
-        format.xml{ render :xml=>failure_message, :root => :errors}
-        format.json{render :json=>failure_message.to_json,:root => :errors}
+        format.xml { render :xml=> self.resource.all_errors.to_xml(:root=>'errors') }
+        format.json { render :json=> self.resource.all_errors }
       end
     end
-  end 
+  end
+
 end

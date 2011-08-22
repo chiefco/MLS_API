16462d5f01a048d500a7c29b6ff89738a9cff46d
railsfactory-sriman <sriman@railsfactory.org>
2011-08-22 16:50:43 +0530

    added relation in bookamrk

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,5 +3,5 @@ class Bookmark
   include Mongoid::Timestamps
   field :name, :type => String
   validates_presence_of :name, :message=>"name - Required parameter missing", :code=>"2009"
-  references_many=>:bookmarked_contents,:dependent=>:destroy
+  references_many :bookmarked_contents,:dependent=>:destroy
 end

246b460573bca43efbb4e33d86a35a2947381fb0
railsfactory-soundar <soundar.rathinasamy@sedin.co.in>
2011-08-26 18:57:17 +0530

    attachments list API call added

--- app/controllers/v1/passwords_controller.rb
+++ app/controllers/v1/passwords_controller.rb
@@ -3,7 +3,8 @@ class V1::PasswordsController < Devise::PasswordsController
 
 
   def create
-    self.resource = resource_class.send_reset_password_instructions(params[resource_name])
+    pa
+    self.resource = resource_class.send_reset_password_instructions(params[:user])
     if successful_and_sane?(resource)    
       success_message=success.merge(RESET_TOKEN_SENT)
       respond_to do |format|
@@ -20,25 +21,22 @@ class V1::PasswordsController < Devise::PasswordsController
   end
   
   def update
-    self.resource = User.where(reset_password_token: params[resource_name][:reset_password_token]).first
-    if self.resource
-      if password_updated?
-        self.resource.update_attribute("reset_password_token",nil)
-        respond_to do |format|
+    self.resource = User.where(reset_password_token: params[:user][:reset_password_token]).first
+    
+    respond_to do |format|
+      if self.resource
+        if password_updated?
+          self.resource.update_attribute("reset_password_token",nil)
           format.xml{ render :xml=>success, :root => :result}
           format.json{render :json=>success.to_json}
-        end
-      else
-        respond_to do |format|
+        else
           format.xml { render :xml=> self.resource.all_errors.to_xml(:root=>'errors') }
           format.json { render :json=>failure.merge(:errors=>self.resource.all_errors).to_json }
         end
-      end
-    else
-      respond_to do |format|
-        format.xml { render :xml=> self.resource.all_errors.to_xml(:root=>'errors') }
-        format.json { render :json=>failure.merge(:errors=>[{:code=>3070, :message=>"password reset token - Invalid Parameter"}]).to_json }
-      end
+      else
+          format.xml { render :xml=> self.resource.all_errors.to_xml(:root=>'errors') }
+          format.json { render :json=>failure.merge(:errors=>[{:code=>3070, :message=>"password reset token - Invalid Parameter"}]).to_json }
+      end 
     end 
   end
   
@@ -46,8 +44,8 @@ class V1::PasswordsController < Devise::PasswordsController
   
   def password_updated?
     self.resource.set_password = true
-    self.resource.password = params[resource_name][:password] if params[resource_name][:password]
-    self.resource.password_confirmation = params[resource_name][:password_confirmation] if params[resource_name][:password_confirmation]
+    self.resource.password = params[:user][:password] if params[:user][:password]
+    self.resource.password_confirmation = params[:user][:password_confirmation] if params[:user][:password_confirmation]
     self.resource.save
   end 
 
--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -7,7 +7,8 @@ class Attachment
   validates_presence_of :attachable_id, :message=>"attachable_id - Blank Parameter", :code=>3034
   validates_presence_of :attachable_type, :message=>"attachable_type - Blank Parameter", :code=>3022
   validates_inclusion_of :attachable_type, :in=>["User","Item","Category","Page"], :message=>"attachable_type - Invalid Parameter", :code=>3050
-  
+  SORT_BY_ALLOWED = [:file_name, :size, :content_type]
+  ORDER_BY_ALLOWED =  [:asc,:desc]
   before_save :generate_link
   field :file_name, type: String
   field :file_type, type: String
@@ -20,6 +21,20 @@ class Attachment
   protected
   def generate_link
     self.file_link = self.file.url
+  end 
+  
+  def self.list(attachments,params,paginate_options)
+    params[:sort_by] = 'created_at' if params[:sort_by].blank? || !SORT_BY_ALLOWED.include?(params[:sort_by].to_sym)
+    params[:order_by] = 'desc' if params[:order_by].blank? || !ORDER_BY_ALLOWED.include?(params[:order_by].to_sym)
+    query = 'attachments'
+    query +=  '.where(file_type: params[:file_type])' if params[:file_type]
+    query += '.any_of(self.get_criteria(params[:q]))' if params[:q]
+    query += '.order_by([params[:sort_by],params[:order_by]]).paginate(paginate_options)'
+    eval(query)
+  end
+  
+  def self.get_criteria(query)
+    [ {file_name: query} , { size: query }, { content_type: query }]
   end 
   
 end

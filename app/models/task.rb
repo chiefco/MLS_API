84935c7d68a458ce173b00595fd0dba558489c2b
railsfactory-sriman <sriman@railsfactory.org>
2011-08-29 18:58:52 +0530

    added search functionality in tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,8 @@
 class Task
   include Mongoid::Document
   include Mongoid::Timestamps
+  SORT_BY_ALLOWED = [ :due_date, :is_completed, :description]
+  ORDER_BY_ALLOWED =  [:asc,:desc]
   field :due_date, :type => Time
   field :is_completed,:type=> Boolean,:default=>false
   field :assignee_id,:type=> String
@@ -11,5 +13,16 @@ class Task
   validates_presence_of :description,:code=>"3014",:message=>"description - Blank Parameter"
   def task_item
     {:item=>self.item.nil?  ? "nil" : self.item.to_json(:only=>[:_id,:name])}
+  end
+  
+  def self.list(params,paginate_options,user)
+    p @current_user
+    params[:sort_by] = 'created_at' if params[:sort_by].blank? || !SORT_BY_ALLOWED.include?(params[:sort_by].to_sym)
+    params[:order_by] = 'desc' if params[:order_by].blank? || !ORDER_BY_ALLOWED.include?(params[:order_by].to_sym)
+    if params[:q]
+      user.tasks.any_of(self.get_criteria(params[:q])).order_by([params[:sort_by].to_sym,params[:order_by].to_sym]).paginate(paginate_options)
+    else
+      user.tasks.order_by([params[:sort_by].to_sym,params[:order_by].to_sym]).paginate(paginate_options)
+    end 
   end
  end

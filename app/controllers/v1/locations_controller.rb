abd1820006547d8170321ed3f7a497209bf96948
senthilkumar <senthilkumar@sedin.co.in>
2011-08-29 13:04:40 +0530

    responses format changed

--- app/controllers/v1/locations_controller.rb
+++ app/controllers/v1/locations_controller.rb
@@ -1,17 +1,20 @@
 class V1::LocationsController < ApplicationController
   before_filter :authenticate_request!
-  before_filter :find_location,:only=>[:show,:update,:delete]
+  before_filter :find_location,:only=>[:show,:update,:destroy]
+  LOCATION=[:id,:name,:latitude,:longitude]
   
   def index
     @locations = @current_user.locations
     respond_to do |format|
-      format.xml  { render :xml => @locations }
+      format.xml  { render :xml => @locations,:root=>:result }
+      format.json  { render :json => @locations }
     end
   end
 
   def show
     respond_to do |format|
-      format.xml  { render :xml => @location }
+      format.xml  { render :xml => single_response.to_xml(:root=>:result) }
+      format.json  { render :json => single_response.to_json }
     end
   end
 
@@ -19,9 +22,11 @@ class V1::LocationsController < ApplicationController
     @location = @current_user.locations.build(params[:location])
     respond_to do |format|
       if @location.save
-        format.xml  { render :xml => @location, :status => :created, :location => @location }
+        format.xml  { render :xml => single_response.to_xml(:root=>:result) }
+        format.json  { render :json => single_response.to_json  }
       else
-        format.xml  { render :xml => @location.all_errors, :status => :unprocessable_entity }
+        format.xml  { render :xml => @location.all_errors, :status => :unprocessable_entity, :root=>:result }
+        format.json  { render :json => @location.all_errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -29,9 +34,11 @@ class V1::LocationsController < ApplicationController
   def update
     respond_to do |format|
       if @location.update_attributes(params[:location])
-        format.xml  { head :ok }
+        format.xml  { render :xml =>single_response.to_xml , :root=>:result}
+        format.json  { render :json =>single_response.to_json}
       else
-        format.xml  { render :xml => @location.all_errors, :status => :unprocessable_entity }
+        format.xml  { render :xml => @location.all_errors, :status => :unprocessable_entity, :root=>:result }
+        format.json  { render :xml => @location.all_errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -39,11 +46,20 @@ class V1::LocationsController < ApplicationController
   def destroy
     @location.destroy
     respond_to do |format|
-      format.xml  { head :ok }
+      format.xml  {render :xml=>success, :root=>:result }
+      format.json  {render :json=>success }
     end
   end
   
   def find_location
     @location = @current_user.locations.find(params[:id])
+  end
+  
+  def single_response
+    {:response=>:success,:location=>@location}
+  end
+  
+  def multi_result
+    {:response=>:success,:location=>@locations}
   end
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -10,14 +10,29 @@ class Location
   geocoded_by :name 
   #~ after_validation :geocode 
   before_save :find_co_ordinates
+  ALLOWED_FIELDS=[:_id,:name,:latitude,:longitude]
+  default_scope :only=>ALLOWED_FIELDS
+
   
   def find_co_ordinates
-    latitude,longitude=Geocoder.coordinates(self.name) unless self.latitude.nil? || self.longitude.nil?
-    self.latitude=self.compass_point(latitude) unless latitude.nil?
-    self.longitude=self.compass_point(longitude) unless longitude.nil?
+    latitude,longitude=Geocoder.coordinates(self.name) if self.latitude.nil? || self.longitude.nil?
+    self.latitude="#{latitude} #{compass_point(latitude)}" unless latitude.nil?
+    self.longitude="#{longitude} #{compass_point(longitude)}" unless longitude.nil?
   end
   
   def compass_point(value)
     Geocoder::Calculations.compass_point(value)
+  end
+  
+  def to_xml(options={})
+    options[:only]=ALLOWED_FIELDS
+    super(options)
+  end
+  
+  def to_json(options={})
+    options[:only]=ALLOWED_FIELDS
+    options[:only].delete(:_id)
+    options[:methods]=[:id]
+    super(options)
   end
 end

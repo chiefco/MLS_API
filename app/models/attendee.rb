76689ac595b81507e8cb6da5d72f150d661fbee2
senthilkumar <senthilkumar@sedin.co.in>
2011-08-29 15:30:49 +0530

    changed the xml and json response

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -5,6 +5,16 @@ class Attendee
   field :last_name,:type=>String
   referenced_in :item
   validates_presence_of :first_name,:code=>"3010",:message=>"first_name - Blank Parameter"
-  validates_presence_of :item_id,:code=>"3010",:message=>"first_name - Blank Parameter"
-  #~ validates_uniqueness_of:item_id,:code=>:3010,:message=>"first_name - Blank Parameter"
+  validates_presence_of :item_id,:code=>"3026",:message=>"item_id - Blank Parameter"
+
+  
+  def to_json(options={})
+    options[:only]=[:_id,:first_name,:last_name]
+    super(options)
+  end
+  
+  def to_xml(options={})
+    options[:only]=[:_id,:first_name,:last_name]
+    super(options)
+  end
 end

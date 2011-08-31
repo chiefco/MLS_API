12ea8c25a637a253300a992be3e66d7e7c75d59c
senthilkumar <senthilkumar@sedin.co.in>
2011-08-31 12:00:56 +0530

    CRUD operations for search

--- app/controllers/v1/searches_controller.rb
+++ app/controllers/v1/searches_controller.rb
@@ -2,25 +2,29 @@ class V1::SearchesController < ApplicationController
   before_filter :authenticate_request!
   before_filter :find_search,:only=>[:show,:update,:destroy]
   def index
-    @searches = Search.all
+    @searches = @current_user.searches.all
     respond_to do |format|
-      format.xml  { render :xml => @searches }
+      format.xml  { render :xml => multi_success.to_xml(:root=>:result) }
+      format.json  { render :json => multi_success.to_json }
     end
   end
 
   def show
     respond_to do |format|
-      format.xml  { render :xml => @search }
+      format.xml  { render :xml => render_success.to_xml(:root=>:result) }
+      format.json  { render :json => render_success }
     end
   end
 
   def create
-    @search = Search.new(params[:search])
+    @search = @current_user.searches.build(params[:search])
     respond_to do |format|
       if @search.save
-        format.xml  { render :xml => @search, :status => :created, :location => @search }
+        format.xml  { render :xml => render_success.to_xml(:root=>:result)}
+        format.json  { render :json => render_success.to_json}
       else
-        format.xml  { render :xml => @search.errors, :status => :unprocessable_entity }
+        format.xml  { render :xml => @search.all_errors.to_xml(:root=>:result), :status => :unprocessable_entity }
+        format.json  { render :json => @search.all_errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -28,9 +32,11 @@ class V1::SearchesController < ApplicationController
   def update
     respond_to do |format|
       if @search.update_attributes(params[:search])
-        format.xml  { head :ok }
+        format.xml  { render :xml => render_success.to_xml(:root=>:result) }
+        format.json  { render :json => render_success.to_json }
       else
-        format.xml  { render :xml => @search.errors, :status => :unprocessable_entity }
+        format.xml  { render :xml => @search.all_errors.to_xml(:root=>:result), :status => :unprocessable_entity }
+        format.json  { render :json => @search.all_errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -38,7 +44,8 @@ class V1::SearchesController < ApplicationController
   def destroy
     @search.destroy
     respond_to do |format|
-      format.xml  { head :ok }
+      format.xml  { render :xml => success }
+      format.json  { render :json => success }
     end
   end
 
@@ -49,6 +56,14 @@ class V1::SearchesController < ApplicationController
   private
 
   def find_search
-    @search = Search.find(params[:id])
+    @search = @current_user.searches.find(params[:id])
   end
+  
+  def multi_success
+    {:response=>:success,:searches=>@searches}
+  end
+  
+  def render_success
+    {:response=>:success,:search=>@search}
+  end    
 end

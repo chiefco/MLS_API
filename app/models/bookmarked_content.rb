85f81ad869468e304d1522cdf74d9e8d7261673d
railsfactory-sriman <sriman@railsfactory.org>
2011-09-02 17:56:13 +0530

    changes in bookmarks after testing

--- app/models/bookmarked_content.rb
+++ app/models/bookmarked_content.rb
@@ -7,7 +7,7 @@ class BookmarkedContent
   field :position,:type=>String
   field :show_in_quick_links,:type =>Boolean
   referenced_in :bookmark
-  #~ belongs_to :bookmarkable,:polymorphic=>true
-    belongs_to :bookmarkable, polymorphic: true
+  belongs_to :bookmarkable, polymorphic: true
+  validates_inclusion_of :bookmarkable_type, :in=>["Item","Attachment","Page"], :message=>"bookmarkable_type - Invalid Parameter", :code=>3050
 end
 

f1f520e9ab64c8a70d4db41b1e856ee71ac8eff7
railsfactory-sriman <sriman@railsfactory.org>
2011-09-02 12:03:35 +0530

    removed rails best practice errors

--- app/controllers/v1/template_categories_controller.rb
+++ app/controllers/v1/template_categories_controller.rb
@@ -1,9 +1,9 @@
 class V1::TemplateCategoriesController < ApplicationController
+  before_filter :find_template_category,:only=>[:show,:update,:destroy]
   # GET /template_categories
   # GET /template_categories.xml
   def index
     @template_categories = TemplateCategory.all
-
     respond_to do |format|
       format.xml  { render :xml => @template_categories }
       format.json{render :json=>@template_categories}
@@ -13,8 +13,6 @@ class V1::TemplateCategoriesController < ApplicationController
   # GET /template_categories/1
   # GET /template_categories/1.xml
   def show
-    @template_category = TemplateCategory.find(params[:id])
-
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @template_category }
@@ -22,27 +20,10 @@ class V1::TemplateCategoriesController < ApplicationController
     end
   end
 
-  # GET /template_categories/new
-  # GET /template_categories/new.xml
-  def new
-    @template_category = TemplateCategory.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @template_category }
-    end
-  end
-
-  # GET /template_categories/1/edit
-  def edit
-    @template_category = TemplateCategory.find(params[:id])
-  end
-
   # POST /template_categories
   # POST /template_categories.xml
   def create
     @template_category = TemplateCategory.new(params[:template_category])
-
     respond_to do |format|
       if @template_category.save
         format.html { redirect_to(@template_category, :notice => 'Template category was successfully created.') }
@@ -57,8 +38,6 @@ class V1::TemplateCategoriesController < ApplicationController
   # PUT /template_categories/1
   # PUT /template_categories/1.xml
   def update
-    @template_category = TemplateCategory.find(params[:id])
-
     respond_to do |format|
       if @template_category.update_attributes(params[:template_category])
         format.html { redirect_to(@template_category, :notice => 'Template category was successfully updated.') }
@@ -73,12 +52,13 @@ class V1::TemplateCategoriesController < ApplicationController
   # DELETE /template_categories/1
   # DELETE /template_categories/1.xml
   def destroy
-    @template_category = TemplateCategory.find(params[:id])
     @template_category.destroy
-
     respond_to do |format|
       format.html { redirect_to(template_categories_url) }
       format.xml  { head :ok }
     end
+  end
+  def find_template_category
+    @template_category = TemplateCategory.find(params[:id])
   end
 end

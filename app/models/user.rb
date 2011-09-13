8dc9662854b79e8519e207b956af0f4458a8f73f
railsfactory-sriman <sriman@railsfactory.org>
2011-09-13 10:30:09 +0530

    added validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,6 +30,12 @@ class User
   validates_confirmation_of :password, :message=>"password and password_confimation does not match", :code=>2040, :if=>:pass_create_or_update?
   validates_length_of       :password, :message=>"password- Too short(Minimum is 6 characters)", :minimum => 6, :code=>2038, :allow_blank => true, :if=>:pass_create_or_update?
   validates_length_of       :password, :message=>"password- Too long(Maximum is 128 characters)", :maximum =>128, :code=>2039, :allow_blank => true, :if=>:pass_create_or_update?
+  #~ validates :first_name ,:length => { :minimum => 1 ,:maximum =>40,:message=>"first_name-invalid length",:code=>3074}
+  #~ validates :last_name ,:length => { :minimum => 1 ,:maximum =>40,:message=>" last_name-invalid length",:code=>3075}
+  #~ validates :company ,:length => { :minimum => 1 ,:maximum =>80,:message=>"company-invalid length",:code=>3077}
+  #~ validates :job_title ,:length => {:maximum =>80,:message=>"job_title-invalid length",:code=>3078}
+  #~ validates :business_unit ,:length => {:maximum =>80,:message=>"business_unit-invalid length",:code=>3076}
+
 
   field :first_name, :type=> String
   field :last_name, :type=>String

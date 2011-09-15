81982aa0947bd49e9c3e31672b0f7f555c9163a0
senthilkumar <senthilkumar@sedin.co.in>
2011-09-15 14:59:17 +0530

    methods to find the custom page added

--- app/models/template.rb
+++ app/models/template.rb
@@ -13,4 +13,43 @@ class Template
   references_many :template_categories, :dependent => :destroy
   has_one :item
   default_scope :without=>[:created_at,:updated_at,:template_category_id]
+  
+  def has_custom_page?
+    has_value?(custom_page_definition)
+  end
+  
+  def has_task_page?
+    has_value?(task_page_definition)
+  end
+
+  def has_topic_page?
+    has_value?(topic_page_definition)
+  end
+  
+  def custom_page_definition
+    template_definitions.excludes(:custom_page_id => nil).first
+  end
+
+  def custom_page_definition
+    template_definitions.excludes(:custom_page_id => nil).first
+  end
+  
+  def custom_page_definition
+    template_definitions.excludes(:custom_page_id => nil).first
+  end
+  
+  def task_page_definition
+    template_definitions.where(:has_task_section=>true).first
+  end
+
+  def topic_page_definition
+    template_definitions.where(:has_task_section=>true).first
+  end
+  
+  def custom_page
+    cus=custom_page_definition
+    page={}
+    cus.custom_page.custom_page_fields.collect{|f| page[f.field_name]=f.default} if cus
+    page
+  end
 end

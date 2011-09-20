7857537f6f5b77ba2d48e7f5d20965cc63f978d0
senthilkumar <senthilkumar@sedin.co.in>
2011-09-20 15:15:13 +0530

    domain name changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ MeetlinkshareApi::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-  config.action_mailer.default_url_options = { :host => 'api-meetlinkshare.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'www.meetlinkshare.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true

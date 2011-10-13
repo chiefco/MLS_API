a371309411b0ccf361e46644cbadc47a23e7509a
railsfactory-sriman <sriman@railsfactory.org>
2011-10-13 11:33:10 +0530

    added role

--- app/models/community_user.rb
+++ app/models/community_user.rb
@@ -1,7 +1,7 @@
 class CommunityUser
   include Mongoid::Document
   include Mongoid::Timestamps
-  field :role_id,:type=>String
+  field :role_id,:type=>String,:default=>0
   field :status,:type=>Boolean,:default=>true
   field :user_id,:type=>String
   referenced_in :community

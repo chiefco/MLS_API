bf4677b8bfec6177937e2986c93be021fd701b26
senthilkumar <senthilkumar@sedin.co.in>
2011-10-13 12:01:47 +0530

    methods to find users in community

--- app/models/community.rb
+++ app/models/community.rb
@@ -10,4 +10,28 @@ class Community
   references_many :community_users
   validates_presence_of :name,:code=>3013,:message=>"name - Blank Parameter"
   scope :undeleted,self.excludes(:status=>false)
+  
+  def active_user_ids
+    community_users.where(:status=>true).map(&:user_id)
+  end
+  
+  def stale_user_ids
+    community_users.where(:status=>false).map(&:user_id)
+  end
+  
+  def user_ids
+    community_users.map(&:user_id)
+  end
+  
+  def members
+    User.find(active_user_ids)
+  end
+  
+  def all_members
+    User.find(user_ids)
+  end
+  
+  def stale_members
+    User.find(stale_user_ids)
+  end
 end

3b2f508871fd64bac0e5b89dafafa1241c79b4c5
railsfactory-sriman <sriman@railsfactory.org>
2011-10-19 10:28:06 +0530

    added response

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -7,7 +7,7 @@ class V1::SessionsController < Devise::SessionsController
     resource = warden.authenticate!(:scope => resource_name, :recall => "V1::Sessions#index")
     respond_to do |format|
       format.xml{ render_for_api :user_with_token, :xml => resource, :root => :user}
-      format.json{render_for_api :user_with_token, :json => resource, :root => :user}
+      format.json{render :json=>{:user=>resource.serializable_hash(:only=>[:_id,:authentication_token,:email,:first_name,:last_name,:job_title,:company,:sign_in_count,:last_sign_in_at,:current_sign_in_at,:date_of_birth,:last_sign_in_ip])}.merge(success)}
     end
   end
   def index

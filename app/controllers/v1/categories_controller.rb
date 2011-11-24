ee2dd38e1289cbee1fc5a0d9a5b459bc8ec35612
railsfactory-sriman <sriman@railsfactory.org>
2011-11-24 17:52:21 +0530

    added categories and items

--- app/controllers/v1/categories_controller.rb
+++ app/controllers/v1/categories_controller.rb
@@ -94,7 +94,7 @@ class V1::CategoriesController < ApplicationController
     @sub_categories = @category.children
   end
   def initialize_string_values
-    @category_val= :meets
+    @category_val= :Meets
     @item_val=:categories_items
   end
 end

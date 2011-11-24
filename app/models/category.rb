d0c9fcef38cecce2452e13b3194332ca31de00f0
railsfactory-sriman <sriman@railsfactory.org>
2011-11-24 17:45:07 +0530

    added categories and items

--- app/models/category.rb
+++ app/models/category.rb
@@ -56,7 +56,7 @@ class Category
   end
   
   def category_items(text)
-    {text=>self.items.serializable_hash(:except=>:category_ids)}
+    [{text=>self.items.serializable_hash(:except=>:category_ids)}]
   end
   def sub_categories(text1,text2)
     {text1=>self.items.serializable_hash(:except=>:category_ids),text2=>self.children}

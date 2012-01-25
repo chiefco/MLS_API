a8b31330731e2362cc1cb765c5c1131b9e7de209
railsfactory-senthilkumar <elayaraja@railsfactory.org>
2012-01-25 15:29:53 +0530

    minor changes

--- app/controllers/v1/attachments_controller.rb
+++ app/controllers/v1/attachments_controller.rb
@@ -13,7 +13,7 @@ class V1::AttachmentsController < ApplicationController
     @attachments = Attachment.list(@current_user.attachments,params,paginate_options)
     @count = @current_user.attachments.count
     respond_to do |format|
-      format.json  { render :json => { :attachments=>@attachments.to_json(:only=>[:_id, :file_name, :file_type, :size, :content_type,:file,:created_at]).parse ,:total=>@count}.to_success }
+      format.json  { render :json => { :attachments=>@attachments.to_json(:only=>[:_id, :file_name, :file_type, :size, :content_type,:file,:created_at,:user_id]).parse ,:total=>@count}.to_success }
       format.xml  { render :xml => @attachments.to_xml(:only=>[:_id, :file_type, :file_name, :size,  :content_type]).as_hash.to_success.to_xml(ROOT) }
     end
   end
@@ -73,7 +73,7 @@ class V1::AttachmentsController < ApplicationController
       @attachments = Attachment.list(@current_user.attachments,params,{:page =>1, :per_page => 10})
       @count = @current_user.attachments.count
     respond_to do |format|
-      format.json  { render :json => { :attachments=>@attachments.to_json(:only=>[:_id, :file_name, :file_type, :size, :content_type,:file,:created_at]).parse ,:total=>@count}.to_success }
+      format.json  { render :json => { :attachments=>@attachments.to_json(:only=>[:_id, :file_name, :file_type, :size, :content_type,:file,:created_at, :user_id]).parse ,:total=>@count}.to_success }
       format.xml  { render :xml => @attachments.to_xml(:only=>[:_id, :file_type, :file_name, :size,  :content_type]).as_hash.to_success.to_xml(ROOT) }
     end
   end

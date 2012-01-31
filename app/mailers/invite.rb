704f2e1abe5e3855a5cf2a3c5611e5888e52ca24
railsfactory-senthilkumar <elayaraja@railsfactory.org>
2012-01-31 14:01:30 +0530

    mail changes

--- app/mailers/invite.rb
+++ app/mailers/invite.rb
@@ -1,15 +1,16 @@
 class Invite < ActionMailer::Base
   default :from => "MeetLinkShare <info@meetlinkshare.com>"
   
-  def community_invite(inviter,invitation,community)
-    @inviter,@invitee,@community,@token=inviter,invitation.email,community,invitation.invitation_token
+  def community_invite(inviter,invitation,community,host)
+    @inviter,@invitee,@community,@token,@host=inviter,invitation.email,community,invitation.invitation_token,host
     mail(:from=>@inviter,:to=>@invitee,:subject =>"Invitation for virtual team from #{@inviter}")
   end
   
-  def send_invitations(user,email,community_id="", community="")
+  def send_invitations(user,email,community_id="", community="",host)
     @name ="#{user.first_name} #{user.last_name}"
     @community_id = community_id if !community_id.blank?
     @email, @community = email,community
+    @host = host
     mail(:to=>email,:subject =>"Invitation for MeetLinkShare", :reply_to=>"info@meetlinkshare.com")
   end 
   

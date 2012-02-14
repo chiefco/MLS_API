fd3e2e151ae23c31267e4728ad33623001ac13f9
railsfactory-sriman <sriman@railsfactory.org>
2012-02-14 15:47:40 +0530

    minor changes

--- app/models/community_user.rb
+++ app/models/community_user.rb
@@ -8,7 +8,7 @@ class CommunityUser
   belongs_to :user
   
   def self.other_users(user_id)
-    users=where(:user_id =>user_id).map(&:community).select{|c| c.user_id.to_s != user_id && c.status == true}.to_json(:only=>[:_id,:name,:description]).parse
+    users=where(:user_id =>user_id).map(&:community).select{|c| c.user_id != user_id && c.status == true}.to_json(:only=>[:_id,:name,:description]).parse
     return users
   end
 end

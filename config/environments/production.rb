3e4f7d5b039645c7f840db2de40b0869afa83faf
railsfactory-sriman <sriman@railsfactory.org>
2012-03-01 18:56:52 +0530

    minor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,6 +43,7 @@ MeetlinkshareApi::Application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
+   config.action_mailer.delivery_method = :production
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify

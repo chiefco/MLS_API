25589dc1f4cd172a6c1e423bb7b461933c819f71
railsfactory-sriman <sriman@railsfactory.org>
2012-04-06 18:30:22 +0530

    added subscription response

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -329,7 +329,7 @@ class V1::SessionsController < Devise::SessionsController
       logger.info @receipt_value
       logger.info "ddddddddddddddddddddddddddd"
     if @user && @receipt_value
-      expiry_date=Time.at(@receipt["purchase_date_ms"].to_i/1000) 
+      expiry_date=Time.at(@receipt_value["purchase_date_ms"].to_i/1000) 
       @receipt["product_id"]=="meetlinkshareMonthly" ? @user.update_attributes(:expiry_date=>expiry_date+30.days,:subscription_type=>"month") : @user.update_attributes(:expiry_date=>expiry_date+365.days,:subscription_type=>"year")
       logger.info "ggggggggggggggggggggggggggggggggggggg"
       response_values={:product_id=>@receipt_value["product_id"],:transaction_id=>@receipt_value["transaction_id"],:receipt_details=>receipt_response}

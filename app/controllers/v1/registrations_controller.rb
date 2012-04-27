62421b80f4ed0f1360ddb850b62bdf111e6ca57f
Sapthakiri <sapthakiri@railsfactory.org>
2012-04-27 18:39:10 +0530

    Minor

--- app/controllers/v1/registrations_controller.rb
+++ app/controllers/v1/registrations_controller.rb
@@ -261,7 +261,8 @@ class V1::RegistrationsController < Devise::RegistrationsController
   # Called fron methods 'find_activities', 'find_community_activities'
   # Returns the community activities
   def get_activity(activity, item, item_name, comment_item=nil, message=nil)
-    activity_date = (activity.updated_at).to_time.strftime("%Y-%m-%d %H:%M:%S") rescue ''
+    timezone = @current_user.timezone
+    timezone.blank? ? activity_date = activity.updated_at.strftime("%Y-%m-%d %H:%M:%S") : activity_date = activity.updated_at.in_time_zone(timezone).strftime("%Y-%m-%d %H:%M:%S") rescue ''
     @activities=Yamler.load("#{Rails.root.to_s}/config/activities.yml", {:locals => {:username =>@first_name, :item=>item, :item_name=>item_name}})
     @activities_temp=Yamler.load("#{Rails.root.to_s}/config/activities_temp.yml", {:locals => {:username =>@first_name, :item=>item, :item_name=>item_name}})    
     @item << {:user => @first_name, :entity => "#{item}", :entity_name => item_name, :id => activity.entity._id, :type => activity.entity_type,:type_id => activity.entity_id, :message => "#{@activities[activity.action]}", :message_temp => "#{@activities_temp[activity.action]}", :date => activity_date, :local_date => activity.updated_at, :entity_id => activity.entity_id, :action => activity.action, :shared_id => "#{comment_item.nil? ? activity.shared_id : comment_item._id}", :comment_msg => "#{message.nil? ? nil : message}", :page => activity.page_order }    
